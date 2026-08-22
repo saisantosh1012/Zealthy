1268b2e851aed6792db529b181609653771d8153
D SAI SANTOSH <103347778+saisantosh1012@users.noreply.github.com>
2026-08-22 21:16:44 +0530

    Delete app directory

--- app/api/logout/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { clearedCookie } from "../../../lib/auth";
-
-export async function POST() {
-  const res = NextResponse.json({ ok: true });
-  const cookie = clearedCookie();
-  res.cookies.set(cookie.name, cookie.value, cookie);
-  return res;
-}
